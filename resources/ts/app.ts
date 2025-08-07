5e0c006792d97c68297332f7779222a4d587d526
Andy Brown <andrew.ezra.brown@gmail.com>
2025-08-07 15:20:44 -0700

    Add typescript dependency
    Add typescript as a developer dependency, and a tsconfig.json
    Update vite.config.ts to understand the path alias defined in the
    tsconfig.

--- resources/ts/app.ts
+++ resources/ts/app.ts
@@ -1,1 +1,13 @@
-console.log("hello world");
+import { sayHello } from "@/utils/thing";
+
+interface User {
+    name: string;
+    age: number;
+}
+
+const user: User = {
+    name: "John",
+    age: 30,
+};
+
+console.log(sayHello());
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 export default defineConfig({
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./resources/ts"),
+        },
+    },
     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/ts/app.ts"],
