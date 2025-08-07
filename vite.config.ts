b0d3d4d62c5d320d5b48b39806a6195ece010133
Andy Brown <andrew.ezra.brown@gmail.com>
2025-08-07 16:44:58 -0700

    Downgrade to PHP 11 and add Twill

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
+import laravel from "laravel-vite-plugin";
 import path from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
     resolve: {
