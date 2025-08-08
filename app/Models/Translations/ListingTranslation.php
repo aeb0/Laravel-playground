f9e542fdcbf139a6667b75d5ed83cfbae19be9cc
Andy Brown <andrew.ezra.brown@gmail.com>
2025-08-07 20:29:52 -0700

    Make Listing module and two blocks

---
+++ app/Models/Translations/ListingTranslation.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Translations;
+
+use A17\Twill\Models\Model;
+use App\Models\Listing;
+
+class ListingTranslation extends Model
+{
+    protected $baseModuleModel = Listing::class;
+}
